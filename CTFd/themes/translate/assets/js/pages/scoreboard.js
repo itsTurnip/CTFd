5a6aa7e375e4a14f0447d575291f84fbb8627d89
antares387 <73886039+antares387@users.noreply.github.com>
2020-11-12 22:34:57 +0500

    Update scoreboard.js

--- CTFd/themes/translate/assets/js/pages/scoreboard.js
+++ CTFd/themes/translate/assets/js/pages/scoreboard.js
@@ -47,7 +47,7 @@ const buildGraphData = () => {
     const option = {
       title: {
         left: "center",
-        text: "Топ 10 " + (CTFd.config.userMode === "команд" ? "Teams" : "Users")
+        text: "Топ 10 " + (CTFd.config.userMode === "teams" ? "Команд" : "Users")
       },
       tooltip: {
         trigger: "axis",
